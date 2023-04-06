e42588da1a490bfe4b070e4576ad17afcf2f3a2e
Barış Erzen <122172611+bariserzen@users.noreply.github.com>
2023-04-06 03:18:53 +0300

    Landing-page-react
    Landing-page-react

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import Logo from '../images/logo.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+
+
+const Navbar = () => {
+
+  const [click, setClick] = useState(false);
+  
+  const clickHandler = () => {
+    setClick(!click);
+  }
+  
+  return (
+    <div className='navbar' >
+
+       <div className='logo'>
+          <img src={Logo} alt='logo' />
+       </div>
+
+       <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+             <li className='nav-item'><a href='/'>Home</a></li>
+             <li className='nav-item'><a href='/'>About</a></li>
+             <li className='nav-item'><a href='/'>FAQ</a></li>
+             <li className='nav-item'><a href='/'>Contact</a></li>
+             
+       </ul>
+
+       <div className='hamburger' onClick={clickHandler}>
+           {click ? <FaTimes size={30} style={{color:'#f8f8f8'}}/> : 
+                    <FaBars size={30}  style={{color:'#f8f8f8'}}/>}
+       </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
